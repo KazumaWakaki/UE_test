7a61181c0458d07a3d35594b0911007b508aafc5
KazumaWakaki <221037@st.yoshida-g.ac.jp>
2023-12-06 10:23:11 +0900

    けした

--- Source/C_Project02/C_Project02.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "C_Project02.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, C_Project02, "C_Project02" );
